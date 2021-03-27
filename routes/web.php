910a4d67e5d3b10ed8c13b6fea8dbb6d2311a4d2
Zenildo Araújo <zenildo.araujo@zap.intra>
2021-03-27 11:40:17 +0100

    testando

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Models\publicacao;
 Route::resource('/', 'App\Http\Controllers\PublicacaoController');
 
 Route::get('/index', function () {
+    //teste
     return view('admin/layout');
 });
 
